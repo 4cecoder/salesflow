6ce4151751a55d9be00ac9e5e8836afe295c0f35
4ce <88108711+4cecoder@users.noreply.github.com>
2024-12-06 01:58:45 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'i.pravatar.cc',
+      'github.com'
+    ],
+  },
+};
 
 export default nextConfig;
